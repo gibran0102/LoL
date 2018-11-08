a14f8134e73d42828ebc786fc104b27735158bfd
Gibran0102 <gibran0102@gmail.com>
2018-11-07 21:42:59 -0600

    test multiple requests

--- fetch.py
+++ fetch.py
@@ -3,6 +3,7 @@ import requests as rq
 from key import key
 
 class Fetch():
+    
     def __init__(self, name):
         self._name = name
         self.entry_point = 'https://la1.api.riotgames.com'
@@ -10,5 +11,5 @@ class Fetch():
     
     def get_user_info(self):
         request = rq.get('{}/lol/summoner/v4/summoners/by-name/{}?{}'.format(self.entry_point, self._name, self.api_key))
-        
+
         return request.json()
--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from fetch import Fetch
 
 
 def main(): 
-    name = input('Nombre del invocador (solo LAN): ')
-
-    new_search = Fetch(name)
-
-    infor_user = new_search.get_user_info()
-
-    print(infor_user['name'])
+    #name = input('Nombre del invocador (solo LAN): ')
+    names = ['Gibran0102', 'Yollowstar']
+    for name in name:
+        new_search = Fetch(name)
+        infor_user = new_search.get_user_info()
+        print(infor_user['id'])
     
 
 if __name__ == '__main__': main()
