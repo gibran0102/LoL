45d95e9d8c2d4fc94ddf6d2c280018b34b904bc7
Gibran0102 <gibran0102@gmail.com>
2018-11-07 23:57:42 -0600

    only ids of the games

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from time import sleep
 import numpy as np
+import pandas as pd 
+from scipy import stats
 
 from summon import Summon
 
 
 def main(): 
     
-    input_name = input('Nombre del invocador (solo LAN): ')
+    input_name = input('Nombre del invocador (solo LAN):  ')
     search = Summon(input_name)
-    
+    search.info_match()
+
+
+
     
 
 
--- summon.py
+++ summon.py
@@ -1,28 +1,29 @@
 import requests as rq
 import numpy as np
+from time import sleep
 
 #Local Files
 from Global import api_key, entry_point
+
+entry_point = entry_point
+api_key = api_key
 
 
 class Summon():
 
     def __init__(self, name): 
-        self.entry_point = entry_point
-        self.api_key = api_key
+        
         self.name = name 
         self.account = None
         self.id = None
-        self.list_matches_ids = []
-        self.champions_used = []
-        self.lanes = []
-
-
+        self.ids = []
+    
         self.init_fetch()    
+        sleep(2)
         self.fetch_matches()
 
     def init_fetch(self): 
-        request = rq.get('{}/lol/summoner/v4/summoners/by-name/{}?{}'.format(self.entry_point, self.name, self.api_key)).json()
+        request = rq.get('{}/lol/summoner/v4/summoners/by-name/{}?{}'.format(entry_point, self.name, api_key)).json()
         
         self.id = request['id']
         self.name = request['name']
@@ -31,13 +32,18 @@ class Summon():
 
 
     def fetch_matches(self):
-        request = rq.get('{}/lol/match/v4/matchlists/by-account/{}?{}'.format(self.entry_point, self.account, self.api_key)).json()
+        request = rq.get('{}/lol/match/v4/matchlists/by-account/{}?{}'.format(entry_point, self.account, api_key)).json()
+        
+        self.ids = [match['gameId'] for match in request['matches']]
+        
 
-        self.list_matches_ids = [match['gameId'] for match in request['matches']]
-        self.champions_used =  [match['champion'] for match in request['matches']]
-        self.lanes = [match['lane'] for match in request['matches']]
+        
 
-        return { 'ids': self.list_matches_ids, 'champions': self.champions_used, 'lanes': self.lanes }
+    
+    def info_match(self): 
+        ids = self.ids[0:5]
+        print(ids)
+
 
     
         
