81965848ebb699cdfbc796d751beeac1391ee2ae
Gibran0102 <gibran0102@gmail.com>
2018-11-07 23:02:32 -0600

    get the data of the game

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from time import sleep
+import numpy as np
+
 from summon import Summon
 
 
 def main(): 
+    
     input_name = input('Nombre del invocador (solo LAN): ')
     search = Summon(input_name)
+    
+    
 
-    matches = search.fetch_matches()
-
-    print(len(matches))
-        
 
 
 if __name__ == '__main__': main()
--- summon.py
+++ summon.py
@@ -14,7 +14,12 @@ class Summon():
         self.account = None
         self.id = None
         self.list_matches_ids = []
+        self.champions_used = []
+        self.lanes = []
+
+
         self.init_fetch()    
+        self.fetch_matches()
 
     def init_fetch(self): 
         request = rq.get('{}/lol/summoner/v4/summoners/by-name/{}?{}'.format(self.entry_point, self.name, self.api_key)).json()
@@ -29,7 +34,11 @@ class Summon():
         request = rq.get('{}/lol/match/v4/matchlists/by-account/{}?{}'.format(self.entry_point, self.account, self.api_key)).json()
 
         self.list_matches_ids = [match['gameId'] for match in request['matches']]
+        self.champions_used =  [match['champion'] for match in request['matches']]
+        self.lanes = [match['lane'] for match in request['matches']]
 
-        return self.list_matches_ids
+        return { 'ids': self.list_matches_ids, 'champions': self.champions_used, 'lanes': self.lanes }
+
+    
         
     
