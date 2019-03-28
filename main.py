e087c7be3e425ec45531efeaaaa9db1cd12e6e2f
Gibran0102 <gibran0102@gmail.com>
2019-03-28 14:24:49 -0600

    test

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from summoner import Summon
 
-
 def main(): 
     input_name = input('name of the summoner (only LAN): ')
-
     summon = Summon(input_name)   
     summon.matchs_ids()
     summon.match_info_data()
