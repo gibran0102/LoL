2827ade428b16f4b87753a2cf5e3a98f9cc84522
Gibran0102 <gibran0102@gmail.com>
2019-03-28 17:49:02 -0600

    .

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ def main():
     
     if sys.argv[1]:
         input_name = sys.argv[1]
-    else:
-        input_name = input('name of the summoner (only LAN): ')
-
+        
     summon = Summon(input_name)   
     summon.matchs_ids()
     summon.match_info_data()
     summon.show_map()
     summon.save_map()
 
-    
+
     
     
 
